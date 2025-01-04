1f1cfb7005ba80e6466e1b318b6537855880d70f
radu-bordea <radu.bordea.dev@gmail.com>
2025-01-04 12:23:33 +0200

    Build a responsive layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,31 @@
-import { Button } from "@chakra-ui/react";
+import { Grid, GridItem } from "@chakra-ui/react";
 
 function App() {
-  return <Button>Click me</Button>;
+  console.log("App Rendered");
+  return (
+    <Grid
+      h="50px"
+      templateRows={{ base: "repeat(2, 1fr)", lg: "repeat(2, 1fr)" }} // Two rows for mobile and desktop
+      templateColumns={{ base: "1fr", lg: "repeat(2, 1fr)" }} // One column on mobile, three on desktop
+      
+    >
+      <GridItem bg="lightcoral" rowSpan={1} colSpan={3}>
+        Nav
+      </GridItem>
+      <GridItem bg="lightblue" rowSpan={1} colSpan={1}>
+        Main
+      </GridItem>
+      {/* Aside will be hidden on mobile by setting display: none on base breakpoint */}
+      <GridItem
+        bg="yellow"
+        rowSpan={1}
+        colSpan={1}
+        display={{ base: "none", lg: "block" }} // Hide aside on mobile, show on larger screens
+      >
+        Aside
+      </GridItem>
+    </Grid>
+  );
 }
 
 export default App;
